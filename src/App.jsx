0f8b37ad1d715c13619feb0fa0ce3b23a113d564
Anderson Suga <anderson.suga@gmail.com>
2023-04-09 19:02:37 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author='Diego Fernandes'
+        content='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+      />
+      <Post
+        author='Gabriel Buzzi'
+        content='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+      />
     </div>
   )
 }
