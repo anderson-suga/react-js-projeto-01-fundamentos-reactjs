646a921475269aa713385f7da04544999145c39f
Anderson Suga <anderson.suga@gmail.com>
2023-04-11 21:27:01 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author='Diego Fernandes'
         content='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
